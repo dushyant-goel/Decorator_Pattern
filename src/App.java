43302cada94c389d5583af0feb2f0a050c97941e
Dushyant Goel <dushyant95goel@gmail.com>
2023-06-12 18:42:53 +0530

    driver code

--- src/App.java
+++ src/App.java
@@ -1,5 +1,11 @@
+import java.util.Scanner;
 public class App {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        
+       VegDelight pizza1 = new VegDelight();
+       ExtraCheese cheesyPizza1 = new ExtraCheese(pizza1);
+
+       System.out.println(cheesyPizza1.cost());
+
     }
 }
