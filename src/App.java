75b147c6281939405dcf146d84f8ec1ff4b17dd9
Dushyant Goel <dushyant95goel@gmail.com>
2023-06-12 18:54:10 +0530

    example

--- src/App.java
+++ src/App.java
@@ -2,10 +2,13 @@ import java.util.Scanner;
 public class App {
     public static void main(String[] args) throws Exception {
         
-       VegDelight pizza1 = new VegDelight();
-       ExtraCheese cheesyPizza1 = new ExtraCheese(pizza1);
+    //    VegDelight pizza1 = new VegDelight();
+    //    ExtraCheese cheesyPizza1 = new ExtraCheese(pizza1);
 
-       System.out.println(cheesyPizza1.cost());
+    //    System.out.println(cheesyPizza1.cost());
+
+       BasePizza pizza2 = new Mushroom(new ExtraCheese(new Farmhouse()));
+       System.out.println(pizza2.cost());
 
     }
 }
